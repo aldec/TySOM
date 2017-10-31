8772961491b4cbb2909c89e4ee9270d697b6ce95
Piotr Czak <p.czak@alltech.gda.pl>
2017-10-31 12:31:19 +0100

    Added new SDx platforms

---
+++ SDx-platforms/2017.2/TySOM_2_7Z045_FMC2_FMC_ADAS/sw/prebuilt/portinfo.h
@@ -0,0 +1,23 @@
+#ifndef _SDS_PORTINFO_H
+#define _SDS_PORTINFO_H
+/* File: /wrk/xsjhdnobkup2/tshui/2017.2_sdx/zc702/TEST_WORK_lnx64/add_gen_prebuilt/apf/_sds/p0/.cf_work/portinfo.h */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void _p0_cf_framework_open(int);
+void _p0_cf_framework_close(int);
+
+#ifdef __cplusplus
+};
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+void switch_to_next_partition(int);
+void init_first_partition();
+void close_last_partition();
+#ifdef __cplusplus
+};
+#endif /* extern "C" */
+#endif /* _SDS_PORTINFO_H_ */
---
+++ SDx-platforms/2017.2/TySOM_2_7Z045_FMC3_FMC_ADAS/sw/prebuilt/portinfo.h
@@ -0,0 +1,26 @@
+#ifndef _SDS_PORTINFO_H
+#define _SDS_PORTINFO_H
+/* File: /wrk/xsjhdnobkup4/xd-relman/2016.4_sdx/ta/results/platforms/prebuilt/generate/zed/TEST_WORK_lnx64/add_gen_prebuilt/apf/_sds/p0/.cf_work/portinfo.h */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void _p0_cf_framework_open(int);
+void _p0_cf_framework_close(int);
+
+#ifdef __cplusplus
+};
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+void switch_to_next_partition(int);
+void init_first_partition();
+void close_last_partition();
+#ifdef __cplusplus
+};
+#endif /* extern "C" */
+#endif /* _SDS_PORTINFO_H_ */
+
+// 67d7842dbbe25473c3c32b93c0da8047785f30d78e8a024de1b57352245f9689
+
